e10b476fcae9d2a712f4d972baf361e3afe25a2a
Arshad Badar Khan <khan@checkmybus.com>
2018-07-08 15:30:33 +0200

    nothing

--- AppVerse.Jewel.HorizonModule/ViewModels/BottomHorizonViewModel.cs
+++ AppVerse.Jewel.HorizonModule/ViewModels/BottomHorizonViewModel.cs
@@ -5,9 +5,9 @@ using Microsoft.Practices.Unity;
 
 namespace AppVerse.Jewel.HorizonModule.ViewModels
 {
-    public class TopHorizonViewModel : BaseViewModel
+    public class BottomHorizonViewModel : BaseViewModel
     {
-        public TopHorizonViewModel(IUnityContainer unityContainer) : base(unityContainer)
+        public BottomHorizonViewModel(IUnityContainer unityContainer) : base(unityContainer)
         {
 
         }
@@ -21,14 +21,18 @@ namespace AppVerse.Jewel.HorizonModule.ViewModels
         public List<List<LengthUnitSystem>> Depth { get; set; }
         public void Initialize(DepthFile depth)
         {
-            
+
             var depthChart = depth.TopHorizon.Depth;
-         
+
             for (int row = 0; row < 26; row++)
             {
                 var rowList = new List<LengthUnitSystem>();
                 for (int column = 0; column < 16; column++)
-                    rowList.Add(depthChart[column][row]);
+
+                {
+                    var depthValue = new LengthUnitSystem(depthChart[column][row].SelectedValue + 328.0);
+                    rowList.Add(depthValue);
+                }
                 Depth.Add(rowList);
             }
         }
